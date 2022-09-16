8d14e7211679034fbb32d972e89f30d7921d53aa
Bilguun2004 <bilguun8689@gmail.com>
2022-09-16 10:47:00 +0800

    Navigation App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { Home } from "./pages/Home";
+import Admin from "./pages/Admin";
+import  Blog from "./pages/Blog";
+import  Blogs  from "./pages/Blogs";
 import { sendRequest } from "./utils/Api";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   // const getData = async () => {
   //   const users = await sendRequest("user", "GET");
   //   console.log(users);
   // };
   // getData();
-  return <>
-  <Home/>
-  </> ;
+  return<BrowserRouter>
+  <Routes>
+    <Route path={'/'} element={<Home/>}/>
+    <Route path={'/admin'} element={<Admin/>}/> 
+    <Route path={'/blog'} element={<Blog/>}/> 
+    <Route path={'/blogs'} element={<Blogs/>}/> 
+  </Routes>
+</BrowserRouter>
 }
 
 export default App;
